fd51f406e852070bac19375ca12b4a28767e88fa
Nitin <nitin8032005@gmail.com>
2025-11-30 15:13:31 +0530

    Restore auth actions

--- components/ui/smooth-scroll.tsx
+++ components/ui/smooth-scroll.tsx
@@ -4,6 +4,7 @@ import { ReactLenis } from '@studio-freight/react-lenis'
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   return (
+    // @ts-ignore - React 19 type mismatch fix
     <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
       {children}
     </ReactLenis>
